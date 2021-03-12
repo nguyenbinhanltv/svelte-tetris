bb2d5e85e58f27f44d3ffb732b7edd81c8e155e5
Nguyen Binh An <nguyenbinhanltv@gmail.com>
2021-03-12 17:32:18 +0700

    update store tetris gameconsole

---
+++ src/services/local-storage.service.svelte
@@ -0,0 +1,15 @@
+<script lang="ts" context="module">
+  const SVELTE_TETRIS_STORAGE_KEY = "SVELTE_TETRIS";
+  export class LocalStorageService {
+    constructor() {}
+
+    static setMaxPoint(max: number) {
+      localStorage.setItem(SVELTE_TETRIS_STORAGE_KEY, `${max}`);
+    }
+
+    static get maxPoint(): number {
+      const max = parseInt(localStorage.getItem(SVELTE_TETRIS_STORAGE_KEY));
+      return Number.isInteger(max) ? max : 0;
+    }
+  }
+</script>
--- src/state/tetris/tetris.store.svelte
+++ src/state/tetris/tetris.store.svelte
@@ -1,12 +1,12 @@
 <script lang="ts" context="module">
   import { Store, StoreConfig } from "@datorama/akita";
-  //   import { PieceFactory } from "@trungk18/factory/piece-factory";
+  import type { PieceFactory } from "../../factory/piece-factory.svelte";
   import { GameState } from "../../interfaces/game-state.svelte";
   import type { Piece } from "../../interfaces/piece/piece.svelte";
   import type { Tile } from "../../interfaces/tile/tile.svelte";
   import { MatrixUtil } from "../../interfaces/matrix.svelte";
   import type { Speed } from "../../interfaces/speed.svelte";
-  //   import { LocalStorageService } from "@trungk18/services/local-storage.service";
+  import { LocalStorageService } from "../../services/local-storage.service.svelte";
 
   export interface TetrisState {
     matrix: Tile[];

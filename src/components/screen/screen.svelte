4eb93488d661eb95f453e27c4c4cb80233f697a7
Nguyen Binh An <nguyenbinhanltv@gmail.com>
2021-03-13 00:32:23 +0700

    making screen gameboard

---
+++ src/components/screen/screen.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+  import { FilledTile } from "../../interfaces/tile/filled-tile.svelte";
+  import Ttile from "../tile/tile.svelte";
+
+  const title = "Svelte Tetris";
+  const filled = new FilledTile();
+</script>
+
+<div class="topBorder">
+  <span class="l mr-10 w-40" />
+  <span class="l mr-10" />
+  <span class="l mr-10" />
+  <span class="l mr-10" />
+  <span class="l mr-10" />
+  <span class="r ml-10 w-40" />
+  <span class="r ml-10" />
+  <span class="r ml-10" />
+  <span class="r ml-10" />
+  <span class="r ml-10" />
+</div>
+<h1>{{ title }}</h1>
+<div class="view">
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <p />
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <p />
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+</div>
+<div class="view l">
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <p />
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <em />
+  <Ttile tile={filled} />
+  <p />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+  <div class="clear" />
+  <Ttile tile={filled} />
+</div>
+
+<style lang="scss">
+  @import "./screen.scss";
+</style>
